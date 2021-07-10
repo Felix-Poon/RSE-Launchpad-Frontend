73ecf6a4c3dd3b3e2ceacedcf0563e474a2957be
Jamie Domingo <jamiedomingo@JCD-Macbook.local>
2021-07-10 16:49:54 +1000

    Structuring folders

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { Homepage } from './pages/Homepage';
+import { Header } from './components/Header';
+import { Login } from './pages/Login';
+import { Register } from './pages/Register';
 
 function App() {
+
   return (
-    <div className="App">
-      Hello world
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          {/* ADD MORE PAGES HERE */}
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/">
+            <Homepage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
