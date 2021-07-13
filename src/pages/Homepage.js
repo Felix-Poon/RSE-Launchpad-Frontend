9c912134c1cb91ca4b13e5bfc38e7ae43e1e3f44
Jamie Domingo <jamiedomingo@JCD-Macbook.local>
2021-07-13 14:54:49 +1000

    adding searchBar code

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,87 @@
+/* eslint-disable no-use-before-define */
 import React from 'react';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: 500,
+    '& > * + *': {
+      marginTop: theme.spacing(3),
+    },
+    backgroundColor: "#FFFFFF",
+    },
+
+  placement: {
+    height: 500,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  iconButton: {
+    padding: 10,
+    float: "left",
+
+  },
+
+  searchArea: {
+    float: "right",
+    width: 450,
+    marginTop: 0
+
+  },
+}));
 
 export function Homepage() {
+  const classes = useStyles();
 
-  return(
-    <p>
-      Home
-    </p>
+  return (
+    <div className={classes.placement}>
+      <div className={classes.root}>
+        <IconButton className={classes.iconButton} aria-label="menu">
+            <SearchIcon />
+          </IconButton>
+        <div className={classes.searchArea}>
+          <Autocomplete
+            multiple
+            id="tags-outlined"
+            options={subjectList}
+            getOptionLabel={(option) => option.title}
+            // defaultValue={[subjectList[5]]}
+            
+            filterSelectedOptions
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Search for resources"
+                placeholder="Selected"
+              />
+            )}
+
+          />
+
+        </div>
+      </div>
+
+    </div>
   );
 }
+
+const subjectList = [
+  { title: 'Accenture'},
+  { title: 'Art'},
+  { title: 'AWS'},
+  { title: 'Books'},
+  { title: 'C++'},
+  { title: 'Engineering'},
+  { title: 'Geography'},
+  { title: 'Plants'},
+  { title: 'Python'},
+  { title: 'React'},
+  { title: 'Stoicism'},
+  { title: 'Software development'},
+];
