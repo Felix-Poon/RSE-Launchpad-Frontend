b2d55f232f833595d9a196aa6e5ee32de05556df
Felix <felixpn066@gmail.com>
2021-07-18 05:08:15 +1000

    show own user resources

--- src/pages/UserResources.jsx
+++ src/pages/UserResources.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
 import { SearchCard } from '../components/SearchCard';
 import { UserContext  } from '../components/UserContext';
+import { Box } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 //import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme) => ({
+  card: {
+    backgroundColor: 'white'
+  },
+  box: {
+    textAlign: 'left',
+    justifyContent: 'left',
+    borderRadius: '5px',
+    boxShadow: '0 3px 5px 2px rgba(0,0,0,.3)',
+    padding: '20px',
+    margin: '30px 40px',
+    width: '55vw',
+    '&:hover': {
+      background: "#c1bfec",       
+      }
+  },
+  cardHeader: {
+    margin: '10px 0',
+  },
+  cardTitle: {
+    margin: 0,
+  },
+  cardText: {
+    margin: 0,
+  },
+  cardAuthor: {
+    margin: '25px 0 0 0',
+    fontSize: '0.75rem'
+  },
+  footer: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  rating: {
+    margin: '20px 0 0 0',
+    float: 'right'
+  }
+}));
+
 export function UserResources() {
+  const classes = useStyles();
   //const classes = useStyles();
   const[resources, setResources] = React.useState([]);
   const context = React.useContext(UserContext);
@@ -17,13 +59,15 @@ export function UserResources() {
   React.useEffect(() => {
     if(firstRender.current) {
       firstRender.current = false;
+    } else {
+      console.log("res: ", resources[0].ID);
     }
-    console.log("res: ", resources);
   },[resources])
 
 
   React.useEffect(() => {
     getResources();
+    console.log(usernameToken)
   },[])
 
   async function getResources () {
@@ -45,6 +89,7 @@ export function UserResources() {
         alert(`error: ${response['status']} Failed to fetch`);
       }
     } catch (error) {
+      console.log(error)
       alert("error: ", error)
     }
   }
@@ -55,18 +100,23 @@ export function UserResources() {
         <div style={{margin:'30px 40px'}}>
           <h2>Your Resources</h2>
         </div>
-        {secondLoad && (
+        {firstRender.current === false && (
           <>
             {resources.map((val, idx) => {
-              return <SearchCard 
-                        key={idx}        
-                        title={val['ID']}
-                        link={val['Location']}
-                    />
+              return (
+                <SearchCard 
+                key={idx}
+                title= {val.ID}
+                link={val.Location}
+                text={val.Description}
+                author={val.Author}
+                rating={val.CommunityRatings.Overall}
+              />
+              )
             })}
           </>
         )}
-
+{/* 
         <SearchCard 
           title='TITLE'
           link='linkkk'
@@ -87,7 +137,7 @@ export function UserResources() {
           text='heyyy'
           author='author'
           rating='2'
-        />
+        /> */}
       </div>
     </div>
   );
