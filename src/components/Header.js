b41b0173a6ce27de4ecca709820c02b28c622269
Felix <felixpn066@gmail.com>
2021-07-17 17:11:02 +1000

    show bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -34,7 +34,7 @@ const logoutMenu = (<LogoutMenu/>);
 // Put header here
 export function Header() {
   const classes = useStyles();
-  const [loggedIn, setLoggedIn] = React.useState(true);
+  const [loggedIn, setLoggedIn] = React.useState(false);
 
   /* Check if user logged in */
   let menu = loggedIn ? logoutMenu : loginMenu;
